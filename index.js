d6f41d83a06f36683028fd5f931da5b36e03d28f
min kim <minwookkim2013@u.northwestern.edu>
2018-08-09 15:46:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector){
+  const inp = selector.toString()
+  return document.querySelector(inp).innerHTML
+}
 
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('target').innerHTML
+}
+
+function increaseRankBy(n){
+  const listt = document.getElementById('app').querySelector('ul.ranked-list li')
+  for (let i = 0; i<listt.length; i++){
+    listt[i].innerHTML=parseInt(listt[i].innerHTML)+1
+  }
+}
+
+function deepestChild(){
+  return document.getElementById('grand-node').querySelector('div div div div').innerHTML
+}
